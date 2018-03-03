d2d24b1d9090f5344ec19264b53f430f8f5f2ed7
Aidan Ranney <aidan.s.ranney@gmail.com>
2018-03-02 16:57:10 -0800

    Finished the latest @jembijoux tutorial - also fixed some of my bugs

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app  = express()
 app.use(bodyParser.json())
 
 var mySnake = {
-  color: '#DFFF00',
+  color: '#a87008',
   name: 'Shai Halud',
   head_url: 'http://cf.geekdo-images.com/images/pic1732684_t.png', // dune sandworm
   taunt: "yo let that spice flow",
@@ -13,20 +13,43 @@ var mySnake = {
   tail_type: 'pixel'
 }
 
-var moves = ['up', 'down', 'left', 'right']
-
 // Handle GET
-app.get('/', (req, res) => res.json(mySnake))
+app.get('/', (req, res) => res('<img src="https://monsterlegacy.files.wordpress.com/2014/04/sandwormcropped.jpg"></img>'))
 
 // Handle POST request to '/start'
 app.post('/start', (req, res) => {
-  console.log(req)
+  console.log(req.body)
   res.json(mySnake)
 })
-,
+
 // Handle POST request to '/move'
 app.post('/move', (req, res) => {
-  res.json({'move': moves[Math.floor(Math.random() * 4)]})
+
+  var theGame = req.body
+  var mySnake = theGame.you
+  var snakeHead = mySnake.body.data[0]
+  var food = theGame.food.data[0]
+
+  var move
+
+  if (snakeHead.x - food.x == 0) {
+       if (snakeHead.y - food.y > 0) {
+         move =  'up'
+       } else {
+         move =  'down'
+       }
+  } else {
+      if (snakeHead.x - food.x > 0) {
+           move =  'left'
+        } else {
+          move =  'right'
+        }
+      }
+
+  res.json({
+    'move': move,
+    'taunt': 'waazzzzzupppppp'
+  })
 })
 
 //Handle POST request to ''/end'
